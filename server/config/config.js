54e2a07093ae8d1ddc980fb3ebc52290791e4d49
AllisonRKahn <31288751+AllisonRKahn@users.noreply.github.com>
2017-09-15 16:18:09 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://akahn:1680Ak97@ds123084.mlab.com:23084/class', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
